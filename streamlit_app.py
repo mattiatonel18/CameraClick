733c4e571cc16c45594205ae7ad21b87740e06ff
mattiatonel18 <78115106+mattiatonel18@users.noreply.github.com>
2025-10-30 22:37:11 +0000

    Aggiunta prime cosette

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import math
 
-st.title("🎈 My new app")
+st.title("Calcolo")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    ""
 )
+
+# --- Configurazione pagina ---
+st.set_page_config(
+    page_title="CameraClick",
+    page_icon="📊",
+    layout="centered"
+)
+
+
+# --- Sidebar (opzionale) ---
+# st.sidebar.header("Impostazioni")
+# st.sidebar.info("Aggiungi qui i controlli o le opzioni globali.")
+
+# --- Corpo principale ---
+st.subheader("Sezione principale")
+st.write("Qui potrai inserire i tuoi componenti: input, grafici, tabelle, ecc.")
+
+# --- Footer ---
+st.markdown("---")
+st.caption("Applicazione realizzata con [Streamlit](https://streamlit.io)")
