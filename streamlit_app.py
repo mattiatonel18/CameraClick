c6ef1c1818ea450753db46ac0a4a04389ec6d70e
mattiatonel18 <78115106+mattiatonel18@users.noreply.github.com>
2025-10-31 00:18:18 +0100

    Add sensor type selection and focal length input
    Updated the Streamlit app to include sensor type selection and focal length input.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,13 @@
+# pip freeze > requirements.txt
 import streamlit as st
-import math
+import numpy as np
 
-st.title("Calcolo")
-st.write(
-    ""
-)
+# st.title("Calcolo")
 
 # --- Configurazione pagina ---
 st.set_page_config(
     page_title="CameraClick",
-    page_icon="📊",
+    # page_icon="📊",
     layout="centered"
 )
 
@@ -19,9 +17,27 @@ st.set_page_config(
 # st.sidebar.info("Aggiungi qui i controlli o le opzioni globali.")
 
 # --- Corpo principale ---
-st.subheader("Sezione principale")
-st.write("Qui potrai inserire i tuoi componenti: input, grafici, tabelle, ecc.")
+st.subheader("Caratteristiche macchina e obiettivo")
+
+sensorType = st.radio("Scegliere il tipo di sensore",
+                          ["FullFrame", "APS-C"],
+                          index=1)
+sensorSize = [22.3, 14.9]
+sensorSize.append(np.sqrt(sensorSize[0]**2 + sensorSize[1]**2))
+
+imgSize = [6000, 4000]
+imgSize.append(np.sqrt(imgSize[0]**2 + imgSize[1]**2))
+# st.write(imgSize[2])
+
+lFOC = st.number_input("Lunghezza focale (mm)", value=50, min_value=1, step=5)
+lFOCeff = lFOC
+if sensorType == "APS-C":
+    lFOCeff = lFOC *1.6
+    st.markdown(f":gray[*Lunghezza focale effettiva: {lFOCeff}mm*]")
+
+
+
 
 # --- Footer ---
-st.markdown("---")
-st.caption("Applicazione realizzata con [Streamlit](https://streamlit.io)")
+# st.markdown("---")
+# st.caption("Applicazione realizzata con [Streamlit](https://streamlit.io)")
